43b0299b0761d344a0d3d9ac26b67d86b76d8a72
James Conlon <conlonj25@gmail.com>
2022-10-26 21:41:57 +0200

    fix click on filled squares and after game won

--- src/index.js
+++ src/index.js
@@ -29,6 +29,11 @@ renderSquare(i) {
 
 handleClick(i) {
     const squares = this.state.squares.slice();
+
+    if(calculateWinner(squares) || squares[i]) {
+        return;
+    }
+
     squares[i] = this.state.xIsNext ? "X" : "O";
     this.setState({
         squares: squares,
